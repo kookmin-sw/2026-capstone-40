836a25678710559e5b4517302fa67c8f122fe78a
Eira <eira@nornity.com>
2026-05-08 04:51:13 +0000

    feat: remove app code from page branch

--- utils/basic_webscrapper/src/__init__.py
+++
@@ -1,4 +0,0 @@
-from .core import StealthScraper
-from .config import ScraperConfig
-
-__all__ = ["StealthScraper", "ScraperConfig"]
--- utils/webscrapper/src/__init__.py
+++
@@ -1,5 +0,0 @@
-from .core import StealthScraper
-from .config import ScraperConfig, SchedulerConfig
-from .scheduler import PeriodicScraper
-
-__all__ = ["StealthScraper", "ScraperConfig", "SchedulerConfig", "PeriodicScraper"]
